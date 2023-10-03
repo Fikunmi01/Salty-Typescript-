bdb25caf23606350512aabe6976f159be113e7be
Fikunmi01 <93198530+Fikunmi01@users.noreply.github.com>
2023-10-03 16:28:01 +0100

    Update:Just seeing this project lol

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { Category } from "./components/categories"
+import { Stories } from "./components/experience"
+import { Footer } from "./components/footer"
+import { Hero } from "./components/hero"
+import { Navbar } from "./components/navbar"
+import { PopularDestination } from "./components/popDestination"
+import { Testimonial } from "./components/testimonial"
+import { TopDestination } from "./components/topDestination"
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <Navbar />
+        <Hero />
+        <Category />
+        <Stories />
+        <PopularDestination />
+        {/* <TopDestination destinations={DestinationArray} /> */}
+        <TopDestination/>
+        <Testimonial />
+        <Footer/>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
